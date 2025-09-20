439af7ecc61ccfc958260df84c733ab53c5dd80b
cdy-pc-windows <cdy@mail.sdu.edu.cn>
2025-09-21 00:16:22 +0800

    给老师提交的那板

---
+++ FirstOne/附件/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("We are students.");
+    }
+}
+class Test {
+    public static void main(String[] args) {
+        System.out.println("This is a test class.");
+    }
+}
